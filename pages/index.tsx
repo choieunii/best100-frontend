ab22a60c63195dfeb7df9ebf0bde82fd4ac7fa7f
choieunii <imej221@gmail.com>
2022-06-16 11:19:37 +0900

    add : 초기 베스트 100 페이지

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,54 @@
 /** @jsxImportSource @emotion/react */
-import type { NextPage } from 'next';
+import type { GetServerSideProps, NextPage } from 'next';
 import Head from 'next/head';
 import styled from '@emotion/styled';
 import styles from '../styles/Home.module.css';
 import ListBox from '../components/ListBox';
-import { getBestTop100List } from '../api/best';
+import { getBestTop100List, getCurrent3DaysBestTop5 } from '../api/best';
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import {
     FlexRowCenter,
-    FlexRowSpaceBetween,
     FlexRowStart,
 } from '../styles/theme';
+import { BestTop100Props } from '../api/type';
+import { itemSearch } from '../api/item-info';
 
-const Home: NextPage = () => {
-    const [bestTopList, setBestTopList] = useState<any>();
-    const [sortType, setSortType] = useState<string>("");
+type StyleProps = {
+    sortType: string;
+    thisType: string;
+};
+type Props = {};
+const Home: NextPage<Props> = (props) => {
+    const [bestTopList, setBestTopList] = useState<BestTop100Props[]>();
+    const [sortType, setSortType] = useState<string>('ranking');
+    const [searchValue, setSearchValue] = useState<string>('');
     const router = useRouter();
 
     useEffect(() => {
-        getBestTop100List(sortType).then((res) => setBestTopList(res));
+        if (sortType === 'best5') {
+            getCurrent3DaysBestTop5().then((res) => setBestTopList(res));
+        } else {
+            getBestTop100List(sortType).then((res: BestTop100Props[]) =>{
+                if(sortType === "likeCnt"){
+                    res.sort((a,b) => b.likeCnt - a.likeCnt);
+                }else if(sortType === "replyCnt"){
+                    res.sort((a,b) => b.replyCnt - a.replyCnt);
+                }
+                setBestTopList(res)
+            });
+        }
     }, [sortType]);
 
-    const divs = bestTopList?.map((data: any, i: number) => {
+    const enterKeyPress = (e: any) => {
+        if(e.key == 'Enter'){
+            itemSearch(searchValue).then((res)=>{
+                setBestTopList(res);
+            })
+        }
+    }
+
+    const divs = bestTopList?.map((data: BestTop100Props, i: number) => {
         return <ListBox key={i} {...data} />;
     });
 
@@ -31,10 +57,6 @@ const Home: NextPage = () => {
         new Array(Math.ceil(divs?.length / 5)).fill(null).map((div, i) => {
             return <FlexRow key={i}>{divs?.splice(0, 5)}</FlexRow>;
         });
-    
-    const onClickSortButton = (e:any) => {
-        setSortType(e.target.value);
-    }
 
     return (
         <div className={styles.mcontainer}>
@@ -48,9 +70,75 @@ const Home: NextPage = () => {
                     <CustomLogo src="/img/logo.png" width="180" height="50" />
                 </a>
                 <SortWrapper>
-                    <SortButton onClick={(e)=> {setSortType("")}}>실시간 순위</SortButton>
-                    <SortButton onClick={(e)=> {setSortType("likeCnt")}}>좋아요</SortButton>
-                    <SortButton onClick={(e)=> {setSortType("replyCnt")}}>사용자 의견</SortButton>
+                    <SearchInput
+                        placeholder="검색어 입력 후 ENTER"
+                        value={searchValue}
+                        onChange={(e) => setSearchValue(e.target.value)}
+                        onKeyPress={enterKeyPress}
+                    ></SearchInput>
+                    <SortButton
+                        thisType={'best5'}
+                        sortType={sortType}
+                        onClick={(e) => {
+                            setSortType('best5');
+                        }}
+                    >
+                        HOT
+                    </SortButton>
+                    <SortButton
+                        thisType={'likeCnt'}
+                        sortType={sortType}
+                        onClick={(e) => {
+                            setSortType('likeCnt');
+                        }}
+                    >
+                        좋아요
+                    </SortButton>
+                    <SortButton
+                        thisType={'replyCnt'}
+                        sortType={sortType}
+                        onClick={(e) => {
+                            setSortType('replyCnt');
+                        }}
+                    >
+                        사용자 의견
+                    </SortButton>
+                    <SortButton
+                        thisType={'increaseRank'}
+                        sortType={sortType}
+                        onClick={(e) => {
+                            setSortType('increaseRank');
+                        }}
+                    >
+                        순위 상승
+                    </SortButton>
+                    <SortButton
+                        thisType={'decreaseRank'}
+                        sortType={sortType}
+                        onClick={(e) => {
+                            setSortType('decreaseRank');
+                        }}
+                    >
+                        순위 하락
+                    </SortButton>
+                    <SortButton
+                        thisType={'sale'}
+                        sortType={sortType}
+                        onClick={(e) => {
+                            setSortType('sale');
+                        }}
+                    >
+                        할인 중
+                    </SortButton>
+                    <SortButton
+                        thisType={'ranking'}
+                        sortType={sortType}
+                        onClick={(e) => {
+                            setSortType('ranking');
+                        }}
+                    >
+                        실시간 순위
+                    </SortButton>
                 </SortWrapper>
             </Header>
             {List}
@@ -60,10 +148,19 @@ const Home: NextPage = () => {
         </div>
     );
 };
+
+const SearchInput = styled('input')`
+    border: 1px solid gray;
+    border-radius: 10px;
+    width: 150px;
+    height: 34px;
+    margin-right: 20px;
+    padding-left: 10px;
+`;
 const Header = styled('div')`
     position: relative;
     width: 100%auto;
-    height: 100px;
+    height: 60px;
     line-height: 100px;
     padding-top: 35px;
     padding-bottom: 30px;
@@ -82,19 +179,21 @@ const CustomLogo = styled('img')`
 const SortButton = styled('button')`
     width: 100px;
     height: 35px;
-    background-color: white;
+    background-color: ${(props: StyleProps) =>
+        props.thisType === props.sortType ? 'purple' : 'white'};
     border: 1px solid rgb(54, 76, 99);
-    color: black;
+    color: ${(props: StyleProps) =>
+        props.thisType === props.sortType ? 'white' : 'black'};
     border-radius: 4px;
     cursor: pointer;
-    margin-right: 30px;
+    margin-right: 20px;
 `;
 
 const SortWrapper = styled('div')`
     position: absolute;
     right: 6%;
     ${FlexRowStart}
-    width: 400px;
+    width: 1000px;
 `;
 
 export default Home;
