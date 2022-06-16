eb9d52840f542cfe62ee14937c06a1d8eea0555c
choieunii <imej221@gmail.com>
2022-06-16 11:15:44 +0900

    add : best100 관련 API

--- api/best.ts
+++ api/best.ts
@@ -1,13 +1,30 @@
 import { apiFetch } from './api-fetch';
 import { requestUrl } from './url';
 
-
 export const getBestTop100List = async (sort?: string) => {
     let url = `${requestUrl}/best/top100`;
-    if(sort) url += `?sort=${sort}`;
+    if(sort=== 'likeCnt' || sort === 'replyCnt' || sort ==='ranking') url += `?sort=${sort}`;
+    else if(sort=== 'increaseRank' || sort === 'decreaseRank' || sort ==='sale') url += `?filter=${sort}`;
     return await apiFetch(url, 'get');
 };
 
-export const getBestRankings = async (item_info_id: any) => {
+export const getBestRankings = async (item_info_id: string) => {
     return await apiFetch(`${requestUrl}/best/rankings/${item_info_id}`, 'get');
 };
+
+export const getBestCategory = async (url:string) => {
+    return await apiFetch(`${requestUrl}/best/category${url}`, 'get');
+};
+
+export const getBestBrand = async (url:string) => {
+    return await apiFetch(`${requestUrl}/best/brand${url}`, 'get');
+};
+
+export const getBestByRanking = async (url:string) => {
+    return await apiFetch(`${requestUrl}/best/ranking${url}`, 'get');
+};
+
+export const getCurrent3DaysBestTop5 = async () => {
+    return await apiFetch(`${requestUrl}/best/rankings/top5`, 'get');
+};
+
