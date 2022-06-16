c54d699f51f23ff4bf4aea249219258f095fe38b
choieunii <imej221@gmail.com>
2022-06-16 11:16:44 +0900

    add : API 리턴 타입 정리

---
+++ api/type.ts
@@ -0,0 +1,64 @@
+export type PriceProps = {
+    id: number;
+    price: number;
+    discountRate: number;
+    customerPrice: number;
+    currentUpdate: Date;
+};
+export type RankingProps = {
+    id: number;
+    ranking: number;
+    currentUpdate: Date;
+    countOfItemLikes: number;
+    countOFReplies: number;
+};
+export type ReplyProps = {
+    id: number;
+    content: string;
+    likeCnt: number;
+    hateCnt: number;
+    hasPassword: boolean;
+    currentUpdate: Date;
+};
+
+export type BestTop100Props = {
+    id: number;
+    itemName: string;
+    itemInfoId: string;
+    itemImgUrl: string;
+    likeTopReply: ReplyProps;
+    hateTopReply: ReplyProps;
+    likeCnt: number;
+    replyCnt: number;
+    ranking: RankingProps[];
+    price: PriceProps[];
+};
+
+
+export type BestByRankingProps = {
+    id: number;
+    bestItemInfo: BestTop100Props;
+    currentUpdate: Date;
+};
+
+export type ItemProps = {
+    id: number;
+    brandName: string;
+    cateName: string;
+    currentUpdateBest: boolean;
+    itemImgUrl: string;
+    itemInfoId: string;
+    itemName: string;
+    largeCateName: string;
+    likeCnt: number;
+    replyCnt: number;
+    middleCateName: string;
+    price: PriceProps;
+    ranking: number;
+};
+
+export type updatePostProps = {
+    title: string;
+    text: string;
+    date: string;
+};
